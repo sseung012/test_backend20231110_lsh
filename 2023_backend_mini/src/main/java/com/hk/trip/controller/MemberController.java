cbf5e3f0d467a1e599469094feb62b2884ea33a3
HKEDU <HKEDU@DESKTOP-P7H8U5I>
2023-11-20 14:17:39 +0900

    그건 아니야

--- 2023_backend_mini/src/main/java/com/hk/trip/controller/MemberController.java
+++ 2023_backend_mini/src/main/java/com/hk/trip/controller/MemberController.java
@@ -1,11 +1,16 @@
 package com.hk.trip.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.hk.trip.command.AddUserCommand;
 import com.hk.trip.command.LoginCommand;
 import com.hk.trip.service.MemberService;
 
@@ -21,6 +26,52 @@ public class MemberController {
 
 	@Autowired
 	private MemberService memberService;
+	
+	@GetMapping(value = "/addUser")
+	public String addUserForm(Model model) {
+		System.out.println("회원가입폼 이동");
+		
+		//회원가입폼에서 addUserCommand객체를 사용하는 코드가 작성되어 있어서 
+		//null일경우 오류가 발생하기때문에 보내줘야 함
+		model.addAttribute("addUserCommand", new AddUserCommand());
+		
+		return "member/addUserForm";
+	}
+	
+	@PostMapping(value = "/addUser")
+	public String addUser(@Validated AddUserCommand addUserCommand
+			             ,BindingResult result,Model model) {
+		System.out.println("회원가입하기");
+		
+		if(result.hasErrors()) {
+			System.out.println("회원가입 유효값 오류");
+			return "member/addUserForm";
+		}
+		
+		try {
+			memberService.addUser(addUserCommand);
+			System.out.println("회원가입 성공");
+			return "redirect:/";
+		} catch (Exception e) {
+			System.out.println("회원가입실패");
+			e.printStackTrace();
+			return "redirect:addUser";
+		}
+
+	}
+	
+	@ResponseBody
+	@GetMapping(value = "/idChk")
+	public Map<String,String> idChk(String id){
+		System.out.println("ID중복체크");
+		
+		String resultId=memberService.idChk(id);
+		// json객체로 보내기 위해 Map에 담아서 응답
+		// text라면 그냥 String으로 보내도 됨
+		Map<String,String>map=new HashMap<>();
+		map.put("id", resultId);
+		return map;
+	}
 	
 	//로그인 폼 이동
 	@GetMapping(value = "/login")
@@ -40,7 +91,7 @@ public class MemberController {
 			return "member/login";
 		}
 		
-		String path=memberService.Login(loginCommand, request, model);
+		String path=memberService.login(loginCommand, request, model);
 		
 		return path;
 	}
