af9c3aa4255acf171c80bea39d6c13978a215502
HKEDU <HKEDU@DESKTOP-OFT9IJ9>
2023-12-19 14:05:31 +0900

    update

---
+++ 30_Otter_springboot_jsp/src/main/java/com/hk/otter/config/LoginInterceptor.java
@@ -0,0 +1,30 @@
+package com.hk.otter.config;
+
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class LoginInterceptor implements AsyncHandlerInterceptor {
+	
+@Override
+	public boolean preHandle(HttpServletRequest request, 
+			HttpServletResponse response, Object handler) throws Exception {
+	
+			System.out.println("LoginInterceptor 실행");
+			HttpSession session = request.getSession();
+			if(session.getAttribute("ldto") == null) {
+				System.out.println("로그인 상태가 아닙니다!");
+				response.sendRedirect("/user/signin_form");
+				
+				return false; // false를 작성해야 컨트롤러로 진입을 막을 수 있음
+			}
+		
+			else {
+				return true; // true이면 컨트롤러로 진입함
+			}
+	}	
+	
+
+}
---
+++ 30_Otter_springboot_jsp/src/main/java/com/hk/otter/config/WebConfiguration.java
@@ -0,0 +1,19 @@
+package com.hk.otter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.hk.fintech.config.LoginInterceptor;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer{
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// 추가할 인터셉터 정의
+		registry.addInterceptor(new LoginInterceptor())
+				.addPathPatterns("/banking/*");
+	}
+
+}
