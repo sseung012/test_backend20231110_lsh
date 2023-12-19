40bce339a1212e017cd263a9b723b7937a365f8e
HKEDU <HKEDU@DESKTOP-339OB3C>
2023-12-19 09:55:08 +0900

    컨트롤러 잘못넣음

--- 30_Otter_springboot_jsp/src/main/java/com/hk/otter/controller/FundingController.java
+++ 30_Otter_springboot_jsp/src/main/java/com/hk/otter/controller/FundingController.java
@@ -1,20 +1,7 @@
 package com.hk.otter.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping
+
 public class FundingController {
 
-	@Autowired
-//	private FundingFeign fundingFeign;
-	
-	@GetMapping("/join")
-	public String main() {
-		return "join";
-	}
-	
 }
--- 30_Otter_springboot_jsp/src/main/java/com/hk/otter/controller/MemberController.java
+++ 30_Otter_springboot_jsp/src/main/java/com/hk/otter/controller/MemberController.java
@@ -1,5 +1,20 @@
 package com.hk.otter.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class MemberController {
 
+	@Autowired
+//	private FundingFeign fundingFeign;
+	
+	@GetMapping("/join")
+	public String main() {
+		return "join";
+	}
+	
 }
