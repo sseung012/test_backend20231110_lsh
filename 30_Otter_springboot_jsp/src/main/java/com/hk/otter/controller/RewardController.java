027b2ea126d04893507210cb5df5c354f12a5555
HKEDU <HKEDU@DESKTOP-OFT9IJ9>
2023-12-20 16:53:38 +0900

    update

--- 30_Otter_springboot_jsp/src/main/java/com/hk/otter/controller/RewardController.java
+++ 30_Otter_springboot_jsp/src/main/java/com/hk/otter/controller/RewardController.java
@@ -28,7 +28,7 @@ public class RewardController {
          
          // addCalBoardfForm 페이지에서 유효값 처리를 위해 
          // insertCalCommand 받고 있기 때문에 보내줘야 해!
-         model.addAttribute("insertProductCommand", insertRewardCommand);
+         model.addAttribute("insertRewardCommand", insertRewardCommand);
          return "insertRewardForm";
       }
       
