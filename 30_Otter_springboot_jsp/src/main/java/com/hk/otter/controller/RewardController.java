3f47256e1dc253b7438971c38896ace9c409e797
HKEDU <HKEDU@DESKTOP-OFT9IJ9>
2023-12-20 14:43:36 +0900

    update

--- 30_Otter_springboot_jsp/src/main/java/com/hk/otter/controller/RewardController.java
+++ 30_Otter_springboot_jsp/src/main/java/com/hk/otter/controller/RewardController.java
@@ -3,6 +3,7 @@ package com.hk.otter.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("/reward")
 public class RewardController {
