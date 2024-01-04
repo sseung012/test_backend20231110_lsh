187775ba4b9a370e2539f524e13e8000a16583b8
HKEDU <HKEDU@DESKTOP-OFT9IJ9>
2024-01-04 11:12:13 +0900

    ^0^

--- 30_Otter_springboot_jsp/src/main/java/com/hk/otter/controller/FundingController.java
+++ 30_Otter_springboot_jsp/src/main/java/com/hk/otter/controller/FundingController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hk.otter.dtos.ProductDto;
@@ -27,9 +28,8 @@ public class FundingController {
 	private UserService userService;
 	
 	//펀딩 상품 결제
-  	@GetMapping(value = "/payment")
-//  	@GetMapping(value = "/payment/{seq}")
-  	public String productDetail() {
+  	@PostMapping(value = "/payment")
+  	public String productDetail(String[] count, ) {
 
 //  		System.out.println("펀딩상품보기");
 //  		UserDto ldto = userService.(seq);
