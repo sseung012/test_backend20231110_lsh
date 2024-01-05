2d55f6ea59d511b4b98e4f3d8256653ff3897334
HKEDU <HKEDU@DESKTOP-P7H8U5I>
2024-01-05 10:18:04 +0900

    ordermapper

---
+++ 30_Otter_springboot_jsp/src/main/java/com/hk/otter/mapper/OrderMapper.java
@@ -0,0 +1,14 @@
+package com.hk.otter.mapper;
+
+import java.util.List;
+
+import com.hk.otter.dtos.OrderDto;
+
+public interface OrderMapper {
+
+	static List<OrderDto> paylist() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- 30_Otter_springboot_jsp/src/main/java/com/hk/otter/service/OrderService.java
+++ 30_Otter_springboot_jsp/src/main/java/com/hk/otter/service/OrderService.java
@@ -1,5 +1,15 @@
 package com.hk.otter.service;
 
+import java.util.List;
+
+import com.hk.otter.dtos.OrderDto;
+import com.hk.otter.dtos.ProductDto;
+import com.hk.otter.mapper.OrderMapper;
+
 public class OrderService {
+
+	public static List<OrderDto> paylist() {
+		return OrderMapper.paylist();
+	}
 
 }
