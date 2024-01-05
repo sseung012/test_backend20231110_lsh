8ecefcae650bf853e2862f124d70261d95bd655c
HKEDU <HKEDU@DESKTOP-P7H8U5I>
2024-01-05 11:04:44 +0900

    결제내역

--- 30_Otter_springboot_jsp/src/main/java/com/hk/otter/mapper/OrderMapper.java
+++ 30_Otter_springboot_jsp/src/main/java/com/hk/otter/mapper/OrderMapper.java
@@ -1,14 +1,14 @@
 package com.hk.otter.mapper;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 
 import com.hk.otter.dtos.OrderDto;
 
+@Mapper
 public interface OrderMapper {
 
-	static List<OrderDto> paylist() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	//내가 펀딩참여한 프로젝트 표로 보여주기
+	public List<OrderDto> paylist(String userID);
 
 }
