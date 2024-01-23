a102188332679488b73590922aa8d638ac3e42eb
HKEDU <HKEDU@DESKTOP-339OB3C>
2024-01-23 15:33:35 +0900

    springboot restAPI 프론트

---
+++ 20_frontend_hkboard_springboot_restAPI/src/main/java/com/hk/board/controller/BoardController.java
@@ -0,0 +1,19 @@
+package com.hk.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+	
+	@GetMapping("/")
+	public String home() {
+		System.out.println("HOME 페이지 입니다.");
+		return "home";
+	}
+	@GetMapping("/board")
+	public String board() {
+		System.out.println("게시판 페이지 입니다.");
+		return "board/boardList";
+	}
+}
