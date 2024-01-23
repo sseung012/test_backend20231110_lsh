cfe29adeffb9fa1f74e2f51e10cf85b6da4d6cda
HKEDU <HKEDU@DESKTOP-339OB3C>
2024-01-23 15:34:00 +0900

    핀테크 오픈뱅킹

---
+++ 21_fintech_openbanking_springboot_jsp/src/main/java/com/hk/fintech/config/WebConfiguration.java
@@ -0,0 +1,16 @@
+package com.hk.fintech.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		//추가할 인터셉터 정의
+		registry.addInterceptor(new LoginInterceptor())
+				.addPathPatterns("/banking/*");
+	}
+}
